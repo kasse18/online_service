c20455a0af288461054c70ba8b75442a7ca644d8
kasse18 <fivstudio123@gmail.com>
2023-11-19 14:24:06 +0300

    updated crud

--- database/CRUD.py
+++ database/CRUD.py
@@ -19,32 +19,102 @@ from . import models, schemas
 #     db.refresh(db_user)
 #     return db_user
 
+# USER CRUD
+
+#GET
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+#GET ALL
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
+#CREATE
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "hashed"
-    db_user = models.User(login=user.login, password=fake_hashed_password)
+    db_user = models.User(**user.model_dump())
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
-def update_user(db: Session, user: schemas.UserUpdate):
-    fake_hashed_password = user.password + "hashed"
-    db_user = models.User(login=user.login, password=fake_hashed_password)
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+#UPDATE
+def update_user(db: Session, user_id: int, user: schemas.UserUpdate):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user:
+        for key, value in user.dict(exclude_unset=True).items():
+            setattr(db_user, key, value)
+        db.commit()
+        db.refresh(db_user)
     return db_user
 
-def create_user_tag(db: Session, item: schemas.TagCreate, user_id: int):
-    db_item = models.Tag(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
+#DELETE
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
 
+
+
+# POST CRUD
+
+#GET
+def get_post(db: Session, post_id: int):
+    return db.query(models.Post).filter(models.Post.id == post_id).first()
+
+#GET ALL
+def get_posts(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Post).offset(skip).limit(limit).all()
+
+#CREATE
+def create_post(db: Session, post: schemas.PostCreate):
+    db_post = models.Post(**post.model_dump())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+#UPDATE
+def update_post(db: Session, post_id: int, post: schemas.PostUpdate):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post:
+        for key, value in post.dict(exclude_unset=True).items():
+            setattr(db_post, key, value)
+        db.commit()
+        db.refresh(db_post)
+    return db_post
+
+#DELETE
+def delete_post(db: Session, post_id: int):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post:
+        db.delete(db_post)
+        db.commit()
+    return db_post
+
+# TAG CRUD
+
+#GET
+def get_tag(db: Session, tag_id: int):
+    return db.query(models.Tag).filter(models.Tag.id == tag_id).first()
+
+#GET ALL
+def get_tags(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Tag).offset(skip).limit(limit).all()
+
+#CREATE
+def create_tag(db: Session, tag: schemas.TagCreate):
+    db_tag = models.Tag(**tag.model_dump())
+    db.add(db_tag)
+    db.commit()
+    db.refresh(db_tag)
+    return db_tag
+
+#DELETE
+def delete_tag(db: Session, tag_id: int):
+    db_tag = db.query(models.Tag).filter(models.Tag.id == tag_id).first()
+    if db_tag:
+        db.delete(db_tag)
+        db.commit()
+    return db_tag
